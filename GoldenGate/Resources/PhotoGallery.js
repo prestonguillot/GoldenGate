39daf469ab0182a9a2c8a7bd5e003b1524ff7f44
Preston Guillot <prestonguillot@gmail.com>
2012-09-16 16:23:41 -0700

    show/hide albums jquery

--- GoldenGate/Resources/PhotoGallery.js
+++ GoldenGate/Resources/PhotoGallery.js
@@ -1,5 +1,27 @@
-﻿$(document).ready(function() {
-	$(".albumItem").click(function() {
-		alert("Don't click me yet");
-	});	
+﻿$(function () {
+    $(".albumGroup").slice(1).hide();
+});
+
+$(function () {
+    $(".albumGroup > .albumHeader").filter(":first")
+        .append($("<span>", { id: "showMore" }).click(showMoreAlbums).append("| See More"))
+        .append($("<span>", { id: "showLess"}).click(showLessAlbums).append("| See Less").hide());
+});
+
+function showMoreAlbums() {
+    $("#showMore").hide();
+    $("#showLess").show();
+    $(".albumGroup").slice(1).show(1000);
+}
+
+function showLessAlbums() {
+    $("#showMore").show();
+    $("#showLess").hide();
+    $(".albumGroup").slice(1).hide(1000);
+}
+
+$(function () {
+    $(".albumItem").click(function () {
+        alert("Don't click me yet");
+    });
 });
